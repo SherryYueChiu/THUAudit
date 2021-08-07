bff07af5fce32d0827b8e1532846bf8d2ec4db86
Sherry <sherry@lulainno.com>
2021-08-07 19:56:15 +0800

    更新課表到110年度上學期
    - 改善手動爬蟲抓去取時間的錯誤
    - 暫時取消星期篩選
    - 更新課表到110年度上學期

--- js/classCrawer.js
+++ js/classCrawer.js
@@ -4,35 +4,46 @@ var json = "";
 for (i = 0; i < classCnt; i++) {
     var ccode = "",
         cname = "",
-        croom = "",
-        ctime = "";
+        croom = [],
+        ctime = [];
 
     ccode = $(".button_hilite").eq(i).html().trim(); //課碼
 
     //課名
     if ($("[data-title=課程名稱]").eq(i).html().indexOf("strike") == -1) {
-        nacnameme = $("[data-title=課程名稱]>a").eq(i).html().trim();
+        cname = $("[data-title=課程名稱]>a").eq(i).html().trim();
     } else {
         //已停開
         continue;
     }
 
     //時間地點
-    timeroom = $("[data-title=時間地點]").eq(i).html().trim();
-    if (timeroom.indexOf("星期") != -1) {
-        croom = timeroom.split("[")[1] ? .replace("]", "") || "";
-        ctime = timeroom.split("[")[0] ? .replace("星期", "") ? .split(/[,|\/]/) || "";
-        ctime = `["${ctime?.join('","')}"]`
-    } else {
-        //無公佈時間地點
-        continue;
-    }
+    let timerooms = $("[data-title=時間地點]").eq(i).html().trim().split(" ");
+    timerooms.forEach(timeroom => {
+        if (timeroom.indexOf("星期") != -1) {
+            let str = timeroom;
+            // 教室格式: [教室]
+            let _croom = str.match(/\[(.+)\]/gi)?.[0];
+            if (_croom) {
+                croom.push(_croom.substring(1, _croom.length - 1));
+                // 過濾掉教室和"星期"
+                str = str.replace("星期", "").replace(`[${croom}]`, "");
+            }
+            // 時間格式: 星期/節,節...
+            let _ctime = str.split(/[\/,]/gi, "");
+            if (_ctime) {
+                ctime.push(_ctime?.join('","'));
+            }
+        } else {
+            //無公佈時間地點
+        }
+    });
 
     json += `{
-          "code": "${ccode}",
-          "name": "${cname}",
-          "room": "${croom}",
-          "time": ${ctime}
+          "code": "${ccode || ''}",
+          "name": "${cname || ''}",
+          "room": ["${croom.join("','") || ''}"],
+          "time": ["${ctime.join("','") || ''}"]
   },`;
 }
 console.log(json);
--- js/index.js
+++ js/index.js
@@ -6,26 +6,52 @@ var textPool = {
 
 }
 
-//顯示篩選後課程列表
+// 顯示篩選後課程列表
 function showClass() {
-    classPool.forEach(function (o) {
-
-        //插入課程泡泡
+    classPool.forEach(function (_class) {
+        // 插入課程泡泡
+        // 課名
         let html = `
 <div class="tag bg-dark p-3 mt-3 mb-3 rounded-lg mx-auto shadow-lg w-75">
-	<span><h4 class="color4" style="overflow-x: auto;">${o.name}</h4></span>
-	<hr class="style-one">
-	<span><h5 class="color5">第${o.time.join("、")}節</h5></span>
-	`;
+	<span>
+        <h4 class="color4" style="overflow-x: auto;">
+            ${_class.name}
+        </h4>
+    </span>
+    `;
 
-        o.room.split(",").forEach(function (o2) {
+        // 時間
+        if (_class.time != "") {
             html += `
-		<span class="badge bgColor2 rounded-lg pl-2 pr-2 pb-0" style="border-radius: 10px; color: #333;"><h6>${o2}</h6></span>
+    <hr class="style-one">
+	<span>
+        <h5 class="color5">
+            第${_class.time.join("、")}節
+        </h5>
+    </span>
 	`;
+        }
+
+        // 教室
+        _class.room.forEach(function (_room) {
+            if (_room != "") {
+                html += `
+    <span class="badge bgColor2 rounded-lg pl-2 pr-2 pb-0" style="border-radius: 10px; color: #333;">
+        <h6>
+            ${_room}
+        </h6>
+    </span>
+	`;
+            }
         });
 
+        // 課程代號
         html += `
-	<h6 class="classCode"><a class="color2" href="https://course.thu.edu.tw/view/109/2/${o.code}" target="_blank">${o.code}</a></h6>
+    <h6 class="classCode">
+        <a class="color2" href="https://course.thu.edu.tw/view/109/2/${_class.code}" target="_blank">
+            ${_class.code}
+        </a>
+    </h6>
 </div>`;
 
         $("#classList").append(html);
@@ -33,46 +59,57 @@ function showClass() {
 }
 
 function classFilter(day, time) {
-    //過濾掉不在當天的課
-    classPool = classPool.filter(function (o) {
-        let result = true;
-        //辨識二校教學區
-        if ($("#chooseAceptRegion").val() == "2") {
-            let t = o.room.search(/^(FA|M|MU|PG)\d+$/) != -1;
-            result = (result == t);
-        }
-        if ($("#chooseAceptRegion").val() == "1") {
-            let t = o.room.search(/^(FA|M|MU|PG)\d+$/) == -1;
-            result = (result == t);
-        }
-        return (o.time.indexOf(weekDayWord[day]) != -1) && result;
-    });
-
-    //截取每門課當天上哪幾節
-    classPool = classPool.filter(function (o) {
-        let lBound = 0,
-            rBound = o.time.length - 1;
-        let foundDay = false;
-        o.time.forEach(function (o2, i) {
-            if (!foundDay) {
-                if (o2 == weekDayWord[day]) {
-                    lBound = i;
-                    foundDay = true;
-                }
-            } else {
-                if (o2.charCodeAt() > 1000)
-                    rBound = i - 1;
+    console.warn("all classes", classPool.length)
+    // 過濾掉不在當天的課
+    classPool = classPool.filter(theClass => {
+        let result = false;
+        theClass.room.forEach(_room => {
+            // 辨識二校教學區
+            let isAtSecondArea = _room.search(/^(FA|M|MU|PG)\d+$/) != -1;
+            if ($("#chooseAceptRegion").val() == "2" && isAtSecondArea) {
+                result = true;
+            }
+            if ($("#chooseAceptRegion").val() == "1" && !isAtSecondArea) {
+                result = true;
+            }
+            if ($("#chooseAceptRegion").val() == "0") {
+                result = true;
+            }
+            if (_room == "") {
+                result = true;
             }
         });
-        let ClassTimeOfToday = o.time.slice(lBound + 1, rBound + 1);
-        o.time = ClassTimeOfToday;
-        return ClassTimeOfToday.indexOf(time) != -1;
+        return (theClass.time.indexOf(weekDayWord[day]) != -1) || result;
     });
-    //用課程名稱首字排序
+    console.warn("filter by area", classPool.length)
+
+    // 截取每門課當天上哪幾節
+    // classPool = classPool.filter(theClass => {
+    //     let lBound = 0,
+    //         rBound = theClass.time.length - 1;
+    //     let foundDay = false;
+    //     theClass.time.forEach((_time, i) => {
+    //         if (!foundDay) {
+    //             if (_time == weekDayWord[day]) {
+    //                 lBound = i;
+    //                 foundDay = true;
+    //             }
+    //         } else {
+    //             if (_time.charCodeAt() > 1000)
+    //                 rBound = i - 1;
+    //         }
+    //     });
+    //     let ClassTimeOfToday = theClass.time.slice(lBound + 1, rBound + 1);
+    //     theClass.time = ClassTimeOfToday;
+    //     return ClassTimeOfToday.indexOf(time) != -1;
+    // });
+    console.warn("filter by time", classPool.length)
+
+    // 用課程名稱首字排序
     classPool = classPool.sort(function (a, b) {
         let result = false;
         let AMinusB = a.name.charCodeAt() - b.name.charCodeAt();
-        //中文字優先在前
+        // 中文字優先在前
         if (Math.abs(AMinusB) > 512)
             result |= AMinusB < 0 ? true : false; //中文字對上英文字
         else
@@ -80,13 +117,13 @@ function classFilter(day, time) {
         return result ? 1 : -1;
     });
 
-    //顯示篩選結果
+    // 顯示篩選結果
     showClass();
 }
 
 $(function () {
 
-    //送出選擇
+    // 送出選擇
     $("#page1Submit").click(function () {
         $("#page1").fadeOut(200);
         $("#page2").fadeIn(200);
@@ -94,12 +131,12 @@ $(function () {
     });
 
     function init() {
-        //讀取所有課程
+        // 讀取所有課程
         $.get("./js/allClass.js", function (json) {
             classPool = $.parseJSON(json);
         });
 
-        //取得今天星期幾
+        // 取得今天星期幾
         $("#greetingMsg").html(textPool.greetingMsg.replace("#1 ", weekDayWord[date.getDay()]));
         $("#chooseDay").find(`option[value=${date.getDay()}]`).prop("selected", "selected");
     }
@@ -107,5 +144,5 @@ $(function () {
     init();
 });
 
-//register service worker
+// register service worker
 navigator.serviceWorker.register('service-worker.js', { scope: "." });
